21884d5ebb45d4d19e4256a8b60ff925ee1b1c34
Aoinu <Aoinu607@gmail.com>
2019-03-21 23:59:53 +0900

    Add actors tests

---
+++ __tests__/actors.test.ts
@@ -0,0 +1,45 @@
+"use strict";
+
+import * as ActivityPub from "../src";
+
+describe("Actor", () => {
+  describe("Person", () => {
+    it("should be able to create instance", () => {
+      const actor = new ActivityPub.Person("test");
+      expect(actor).toBeTruthy();
+      expect(actor.name).toBe("test");
+    });
+  });
+
+  describe("Group", () => {
+    it("should be able to create instance", () => {
+      const actor = new ActivityPub.Group("test");
+      expect(actor).toBeTruthy();
+      expect(actor.name).toBe("test");
+    });
+  });
+
+  describe("Service", () => {
+    it("should be able to create instance", () => {
+      const actor = new ActivityPub.Service("test");
+      expect(actor).toBeTruthy();
+      expect(actor.name).toBe("test");
+    });
+  });
+
+  describe("Organization", () => {
+    it("should be able to create instance", () => {
+      const actor = new ActivityPub.Organization("test");
+      expect(actor).toBeTruthy();
+      expect(actor.name).toBe("test");
+    });
+  });
+
+  describe("Application", () => {
+    it("should be able to create instance", () => {
+      const actor = new ActivityPub.Application("test");
+      expect(actor).toBeTruthy();
+      expect(actor.name).toBe("test");
+    });
+  });
+});
